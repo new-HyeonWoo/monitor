5309501d63f14fe539ce27aa3a1074686d4bb858
new-HyeonWoo <chlgusdn3@naver.com>
2020-05-05 03:51:17 +0900

    mq 소켓

--- src/main/java/com/ohap/monitor/mq/client/MessageConsumer.java
+++ src/main/java/com/ohap/monitor/mq/client/MessageConsumer.java
@@ -1,15 +1,27 @@
 package com.ohap.monitor.mq.client;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class MessageConsumer {
 
+	private SimpMessagingTemplate simpMessagingTemplate;
+
+	@Autowired
+	public MessageConsumer(SimpMessagingTemplate simpMessagingTemplate){
+		this.simpMessagingTemplate=simpMessagingTemplate;
+	}
+
 	@JmsListener(destination = "${activemq.queue.name}")
 	public void receive(String message) {
 		log.info("Received Message '{}'", message);
+		String msg = "{\"content\" : \"" + message + "\"}";
+		this.simpMessagingTemplate.convertAndSend("/chart", msg); //메시지 전송
+
 	}
 }
